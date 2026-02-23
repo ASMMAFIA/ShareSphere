43d94766d90a19f55aacd704221a612e5112054f
ASMMAFIA <abedinipourya02@gmail.com>
2026-02-23 12:15:38 +0330

    no flask dependency now it only needs jinja2 to work that's it, also changed the templates a little for static file links to get linked correcty considering the change of httpserver

--- app.py
+++ app.py
@@ -1,85 +1,106 @@
-#!/bin/python3
 import os
+import re
+import shutil
+import urllib.parse
 import argparse
-from utils import fetch_files
+import traceback
+from jinja2 import Environment, FileSystemLoader
+from http.server import HTTPServer, BaseHTTPRequestHandler, HTTPStatus
 from pathlib import Path
-from flask import Flask, request, render_template, send_from_directory, flash, redirect
-
+from utils import *
 
 OS = os.name
+PROGRAM_NAME = 'ShareSphere'
 WINDOWS = OS == 'nt'
 UNIX_LIKE = OS == 'posix'
 SOURCE_FILE_PATH = Path(__file__).resolve().parent
 CURRENT_PATH = os.getcwd()
-TEMPLATE_DIR = f'{SOURCE_FILE_PATH}/templates'
-STATIC_DIR = f'{SOURCE_FILE_PATH}/static'
 USERNAME = os.getlogin()
-PROGRAM_NAME = 'ShareSphere'
 DEFAULT_FOLDER = f"/home/{USERNAME}/Downloads" if UNIX_LIKE else fr'C:\Users\{USERNAME}\Downloads'
+READ_BUFFER = 4096 # bytes
+TEMPLATES_FOLDER = 'templates'
+STATIC_FOLDER = 'static'
+DOWNLOAD_URL = 'download'
+filename_re = re.compile(rb'filename="([^"]+)"')
 
-class ShareSphere:
-  def __init__(self):
-    self.app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
-    self.app.config['UPLOAD_FOLDER'] = DEFAULT_FOLDER
-    self.app.secret_key = 'key'
+class HTTPRequestHandler(BaseHTTPRequestHandler):
+    share_path = DEFAULT_FOLDER
 
-    self.app.add_url_rule('/', 'home', self.home)
-    self.app.add_url_rule('/download/<path:name>', 'download_file', self.download_file)
-    self.app.add_url_rule('/AboutUs', 'about_us', self.about_us)
-    self.app.add_url_rule('/Source', 'source_code', self.source_code)
-    self.app.add_url_rule("/Upload", 'upload', self.upload, methods=['GET', 'POST'])
+    def do_POST(self):
+        if self.path.lower() != '/upload':
+            do_GET()
+            return
+        content_length = int(self.headers['Content-Length'])
+        boundary = self.headers['boundary']
+        body = self.rfile.read(content_length)
+        filename = filename_re.search(body)
+        if filename is None:
+          do_GET()
+          return
+        end_of_form_header = body.find(b'\r\n\r\n')
+        file_obj = body[end_of_form_header+4:]
+        
+        with open(os.path.join(HTTPRequestHandler.share_path, filename.group(1).decode()), 'wb') as f:
+          f.write(file_obj)
 
-  def run(self, share_path=DEFAULT_FOLDER):
-    if share_path[0] == '~' and  UNIX_LIKE:
-      share_path = os.path.join(f'/home/{USERNAME}/' + share_path[2:])
+        self.do_GET()
 
-    elif share_path[0] == '~' and WINDOWS:
-      print('< ~ > operator is not supported on windows')
-      exit(1)
+    def do_GET(self):
+        self.path = self.path[1:] if self.path.startswith('/') else self.path
+        if self.path.startswith('static'):
+            try:
+                with open(self.path, 'rb') as f:
+                    self.send_response(HTTPStatus.OK)
+                    self.end_headers()
+                    shutil.copyfileobj(f, self.wfile)
+            except:
+                self.send_error(HTTPStatus.NOT_FOUND)
+            return
+        elif self.path.startswith('download'):
+          download_path = os.path.join(self.share_path, urllib.parse.unquote(self.path.split('download/')[1]))
+          self.send_response(HTTPStatus.OK)
+          self.send_header('Content-Disposition: attachment;', self.wfile)
+          self.end_headers()
+          with open(download_path, 'rb') as f:
+              shutil.copyfileobj(f, self.wfile)
+          return
 
-    if share_path[0] == '.':
-      share_path = os.path.abspath(CURRENT_PATH) + share_path[1:]
 
-    if not os.path.isdir(share_path):
-      raise 'Directory path is not correct'
-    
-    self.share_path = share_path
-    self.app.run(host='0.0.0.0')
+        jinja_env = Environment(loader = FileSystemLoader(TEMPLATES_FOLDER), auto_reload=True)
+        DOWNLOAD_URL_LEN = len(DOWNLOAD_URL)
+        self.filename = self.path[DOWNLOAD_URL_LEN:] if self.path.startswith(DOWNLOAD_URL) else self.path
+        match self.path.lower():
+            case '':
+                self.filename = 'index.html'
+            case 'upload':
+                self.filename = 'upload.html'
+            case 'aboutus':
+                self.filename = 'about.html'
+            case 'source':
+                self.filename = 'sourcecode.html'
 
-  def home(self):
-    return render_template('index.html', files=fetch_files(self.share_path))
+        template = jinja_env.get_template(self.filename)
+        template = template.render(files=fetch_files(self.share_path))
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        self.wfile.write(template.encode())
+        return
 
-  def download_file(self, name):
-    print(f'Someone is picking up < {name} > in --> {self.share_path}')
-    return send_from_directory(self.share_path, name, as_attachment=True)
-
-  def about_us(self):
-    return render_template('about.html')
-
-  def source_code(self):
-    return render_template('sourcecode.html')
-
-  # single file fetcher (more than one file just get the first file)
-  def upload(self):
-    if request.method == 'GET':
-      return render_template('upload.html')
-    print(request.files)
-    if 'file' not in request.files:
-      flash('No file part')
-      return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-      flash('No selected file')
-      return redirect(request.url)
-    filename = file.filename
-    file.save(os.path.join(self.app.config['UPLOAD_FOLDER'], filename))
-    flash('File uploaded successfully')
-    return redirect('/')
+        try:
+            with open(self.filename, 'rb') as f:
+                self.send_response(HTTPStatus.OK)
+                self.end_headers()
+                shutil.copyfileobj(f, self.wfile)
+        except:
+            print(self.filename)
+            traceback.print_exc()
+            self.send_error(HTTPStatus.NOT_FOUND)
 
 if __name__ == '__main__':
-  a = ShareSphere()
-  parser = argparse.ArgumentParser(prog=PROGRAM_NAME)
-  parser.add_argument('-d', '--directory', action='store', default=DEFAULT_FOLDER)
-  args = parser.parse_args()
-  share_path = args.directory
-  a.run(share_path)
+    parser = argparse.ArgumentParser(prog=PROGRAM_NAME)
+    parser.add_argument('-d', '--directory', action='store', default=DEFAULT_FOLDER)
+    args = parser.parse_args()
+    share_path = args.directory
+    HTTPRequestHandler.share_path = share_path
+    httpd = HTTPServer(('0.0.0.0', 8899), HTTPRequestHandler)
+    httpd.serve_forever()
